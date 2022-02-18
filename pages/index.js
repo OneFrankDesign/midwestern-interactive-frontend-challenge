6fe7df35f2837584c34ea5c9a8bafccad3447013
Frank Krogh <diversifiedground@gmail.com>
2022-02-18 12:48:18 -0600

    Created the box container that holds the main content and its associated styles, started on the custom button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header/Header.component";
+import BoxContainer from "../components/BoxContainer/BoxContainer.component";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <BoxContainer />
     </div>
   );
 }
