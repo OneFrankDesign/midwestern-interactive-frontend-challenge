0115f27f0db358e803461857ff05d9485cb952b9
Frank Krogh <diversifiedground@gmail.com>
2022-02-19 11:45:49 -0600

    created the forms main styles

--- components/ContactForm/ContactForm.component.js
+++ components/ContactForm/ContactForm.component.js
@@ -1,10 +1,51 @@
 import React from "react";
 import styles from "./ContactForm.module.css";
+import CustomButton from "../CustomButton/CustomButton.component";
 
 const ContactForm = () => {
   return (
     <div className={styles.formSection}>
-      <form>Hello</form>
+      <h2 className={styles.subheading}>Heading Two</h2>
+      <form>
+        <div className={styles.formrow}>
+          <div className={styles.formGroup}>
+            <label className={styles.hidden} htmlFor="first_name">
+              First Name
+            </label>
+            <input className={styles.input} type="text" id="first_name" />
+          </div>
+          <div className={styles.formGroup}>
+            <label className={styles.hidden} htmlFor="last_name">
+              Last Name
+            </label>
+            <input className={styles.input} type="text" id="last_name" />
+          </div>
+        </div>
+        <div className={styles.formrow}>
+          <div className={styles.formGroup}>
+            <label className={styles.hidden} htmlFor="title">
+              Title
+            </label>
+            <input className={styles.input} type="text" id="title" />
+          </div>
+          <div className={styles.formGroup}>
+            <label className={styles.hidden} htmlFor="email">
+              email
+            </label>
+            <input className={styles.input} type="text" id="email" />
+          </div>
+        </div>
+        <div className={styles.formrow}>
+          <div className={styles.formGroup}>
+            <label className={styles.hidden} htmlFor="message">
+              Message
+            </label>
+            <textarea className={styles.textArea} id="message" />
+          </div>
+        </div>
+
+        <CustomButton type="submit">Submit</CustomButton>
+      </form>
     </div>
   );
 };
